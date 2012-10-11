7784462f08ac150376bb71744f1af0145e3eb6ac
Timm Kristensen <timmkristensen@gmail.com>
2012-10-11 11:38:41 +0200

    First version

---
+++ README.md
@@ -0,0 +1,91 @@
+S3Client
+============
+
+Simple express site to demonstrate integration to S3.
+
+Description
+-------------
+This is a simple site that demonstrates integration to S3.
+At this point all functionality for S3 is in the S3Client.js file.
+
+
+Usage:
+------
+Usage is fairly simple:
+
+
+
+###Upload:
+    var options = {
+            'key' : keyId,
+            'secret' : secret,
+            'bucket' : bucket,
+            'verb' : 'PUT',
+            'resource' : file.name,
+            'contentType':file.type,
+            'contentLength' : file.size, 
+            'md5' : '',
+            'res' : res,
+            'headers' : {'x-amz-date' : new Date().toUTCString()}
+    };
+    var client = new S3Client(options);
+    client.put(fileToPut, function(err,resp){
+        // do something with response
+    }
+    
+###Delete:
+    var options = {
+            'key' : keyId,
+            'secret' : secret,
+            'bucket' : bucket,
+            'verb' : 'DELETE',
+            'resource' : fileToDelete,
+            'headers' : {'x-amz-date' : new Date().toUTCString()}
+    };
+    var client = new S3Client(options);
+    client.get(function(err,resp){
+        // do something here
+    }
+    
+
+###Get:
+    var options = {
+            'key' : keyId,
+            'secret' : secret,
+            'bucket' : bucket,
+            'verb' : 'GET',
+            'resource' : req.body['query'],
+            'headers' : {'x-amz-date' : new Date().toUTCString()}
+    };
+    var client = new S3Client(options);
+    client.get(function(err,resp){
+        // do something with response 
+        resp.on('data', function(chunk) { // maybe bind to on data to get actual content
+        });
+        
+    });
+
+
+
+TODO
+------
+
+
+NOTES
+Remeber to set bucket policy to public read 
+{
+  "Version":"2008-10-17",
+  "Statement":[{
+    "Sid":"AddPerm",
+        "Effect":"Allow",
+      "Principal": {
+            "AWS": "*"
+         },
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::bucket/*"
+      ]
+    }
+  ]
+}
+
+Direct upload requires s3:putObjectACL for the user that signs the policy for the request   
---
+++ S3Client.js
@@ -0,0 +1,171 @@
+var http = require('http'), 
+	util = require('util'),
+	fs = require('fs'), 
+	crypto = require('crypto'),
+	url = require('url');
+
+var s3_hostname = 's3.amazonaws.com';
+
+var S3_SUB_RESOURCES = ['acl', 'lifecycle', 'location', 'logging', 'notification', 'partNumber', 'policy', 'requestPayment', 
+                        'torrent', 'uploadId', 'uploads', 'versionId', 'versioning', 'versions', 'website'];
+
+var S3Auth = function(options){
+	this.options = options;
+	if(!this.options.date) this.options.date = ''; // prefer x-amz-date
+	if(!this.options.headers) this.options.headers = {'x-amz-date' : this.options.date || new Date().toUTCString()};
+	if(!this.options.headers['x-amz-date']) this.options.headers['x-amz-date'] =  this.options.date || new Date().toUTCString();
+}
+
+/**
+ * 
+ * @returns {String} signed authentication string
+ */
+S3Auth.prototype.createAuth = function(){
+  return 'AWS ' + this.options.key + ':' + this.sign(this.options);
+};
+
+/**
+ * convert URI resource identifier to canonialized resource
+ * 	extract path
+ * 	identify subresource (query paramters)s
+ *  sort 	
+ * 	append subresources 
+ *  
+ * @param {message} the message to sign
+ * @param {options} the options object containing the secret key
+ * @return {String}
+ * @api private 
+ */
+S3Auth.prototype.hmacSha1 = function(message, options){
+	  return crypto.createHmac('sha1', options.secret).update(message).digest('base64');
+}
+/**
+ * StringToSign = HTTP-Verb + "\ n" + Content-MD5 + "\ n" + Content-Type + "\ n" +
+ * Date + "\ n" + CanonicalizedAmzHeaders + CanonicalizedResource;
+ * CanonicalizedResource = [ "/" + Bucket ] + < HTTP-Request-URI, from the
+ * protocol name up to the query string >  + [ sub-resource, if
+ * present. For example "? acl", "? location", "? logging", or "? torrent"];
+ * @return {String}
+ * @api private 
+ */
+S3Auth.prototype.sign = function(options){
+	var stringToSign = this.stringToSign(options);
+	var canonicalizedAmzHeaders = this.constructCanonicalizedAmzHeaders(options.headers);
+	var canonicalizedResource = this.constructCanonicalizedResource(options.resource);
+	stringToSign += canonicalizedAmzHeaders+'\n'; // +CanonicalizedAmzHeaders
+	stringToSign += canonicalizedResource; // + CanonicalizedResource
+	return this.hmacSha1(stringToSign, options);
+}
+/**
+ * 
+ *StringToSign = HTTP-Verb + "\ n" + Content-MD5 + "\ n" + Content-Type + "\ n" +
+* Date + "\ n" + CanonicalizedAmzHeaders + CanonicalizedResource;
+* @return {String}
+* @api private  
+*/
+S3Auth.prototype.stringToSign = function(options){
+	  return [
+	      options.verb 
+	    , options.md5 || ''
+	    , options.contentType || ''
+	    ,  options.headers['x-amz-date'] ? '' : options.date.toUTCString()
+	  ].join('\n')+'\n';
+}
+
+/**
+ *  convert each header name to lowercase
+ *  sort by header name
+ *  combine fields with same name into comma seperated list // todo
+ *  Unfold long headers into one line 
+ *  trim whitespace
+ *  append newline to each header
+ * @param {Object} amzHeaders
+ * @return {String}
+ * @api private
+ */
+S3Auth.prototype.constructCanonicalizedAmzHeaders = function(amzHeaders){
+	var headers = [];
+	for(header in amzHeaders){
+		var val = amzHeaders[header];
+		val.replace(' \n', ' ');//Unfold long headers into one line
+		val = val.trim(); // trim whitespace
+		var amzHeader = header.toLowerCase(); // convert each header name to lowercase
+		headers.push(amzHeader+':'+val);
+	}
+	return headers.sort().join('\n'); // sort and append newline
+}
+
+/**
+ * convert URI resource identifier to canonialized resource
+ * 	extract path
+ * 	identify subresource (query paramters)s
+ *  sort 	
+ * 	append subresources 
+ *  
+ * @param resouce
+ * @return {String}
+ * @api private 
+ */
+S3Auth.prototype.constructCanonicalizedResource = function(resource){
+	var awsurl = url.parse(resource);
+	var subresources = [], canonializedResource = this.options.bucket+(awsurl.pathname || '');
+	if(awsurl.query){
+		awsurl.query.split('&').forEach(function(param){
+			var keyval = param.split('=');
+			if(S3_SUB_RESOURCES.indexOf(keyval[0]) > -1 ){
+				if(keyval.length > 1){
+					subresources.push(keyval[0]+':'+keyval[1]);
+				}else{
+					subresources.push(keyval[0]);
+				}
+			}
+		});	
+	}
+	if(subresources.length>0){
+		canonializedResource += '?'+subresources.sort().join('&');
+	}
+	return canonializedResource;
+}
+
+var S3Client = module.exports =exports = function S3Client(opt){
+	this.options = opt;
+	this.awsAuth=new S3Auth(opt).createAuth();
+}
+
+S3Client.prototype.createRequest = function(cb){
+	var headers = this.options.headers;
+	if(this.options.contentType) headers['content-type'] = this.options.contentType;
+	if(this.options.contentLength) headers['content-length'] =  this.options.contentLength;
+	headers['Authorization'] = this.awsAuth;
+	var request = http.request({
+		'hostname' : s3_hostname,
+		'method' : this.options.verb,
+		'path' :this.options.bucket+this.options.resource,
+		'headers' : headers
+	});
+	request.useChunkedEncodingByDefault = false;
+	request.removeHeader('transfer-encoding');
+	request.on('response', function(res) {
+		if(cb) cb(null, res);
+	});
+	request.on('error', function(err) {
+		if(cb) cb(err);
+	});
+	return request;
+}
+
+S3Client.prototype.put = function(filetosend, cb){
+	console.log("put : "+this.options.resource+' , '+filetosend);
+	var req = this.createRequest(cb);
+	var readStream = fs.createReadStream(filetosend);
+	readStream.pipe(req);
+}
+
+S3Client.prototype.get = function( cb){
+	console.log(this.options.verb +" : "+this.options.resource);
+	
+	var req = this.createRequest(cb);
+	req.end();
+}
+
+
